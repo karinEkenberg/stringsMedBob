2b15add33e0cd272db51cf2d1ab3ccad3b41bb50
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2023-11-29 17:31:22 +0100

    Add files via upload

---
+++ stringsmedbob/stringsmedbob/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringsmedbob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Mitt \"så kallade\" liv"; // använd \ före " för att kunna använda citattecken inuti en mening i c#
+            string myString2 = "När jag vill ha en ny rad\nså fungerar detta"; // \n för en ny rad
+            string myString3 = "Gå till c:\\ drive"; //genom att använda dubbla \ eller 
+            string myString4 = @"Använd ett snabela framför citattecknet :\ drive";
+
+            Console.WriteLine(myString);
+            Console.WriteLine(myString2);
+            Console.WriteLine(myString3);
+            Console.WriteLine(myString4);
+            Console.ReadLine();
+        }
+    }
+}
